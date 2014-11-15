b9227682189e44ff3ce9281ead8233fc96de5ae9
Peter McNamara <pmm9335@rit.edu>
2014-11-15 18:10:13 -0500

    Finished Spiral shooting

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -10,20 +10,20 @@ public class Bullet : MonoBehaviour {
     public float timeAlive;
 
     //Used for curved shots only
-    public float angle = 0;
-    public float rotationalVelocity = 0;
+    //private float angle = 0;
+    //public float rotationalVelocity = 0;
     public float bulletSpeed = 5;
 
 	// Use this for initialization
 	void Start () 
     {
         controller = GetComponent<Controller>();
-        //angle = Vector2.Angle(, target);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        
         controller.Translate(target*bulletSpeed);
 
         if (timeAlive >= lifeTimer)
---
+++ Assets/Scripts/Bullets/Ccw4Spiral.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ccw4Spiral : BulletPattern {
+
+    public float fireAngle = 0;
+    public int increaseAngle = 0;
+    public int increment = 5;
+    
+    //public int numberOfPoints = 4;
+    // Use this for initialization
+    void Start()
+    {
+        //increaseAngle = 360 / numberOfPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(increaseAngle<10)
+        {
+            increaseAngle = 10;
+        }
+        if(fireAngle>360)
+        {
+            fireAngle -= 360;
+        }
+        if (target == null)
+        {
+            target = GameObject.FindWithTag("Player");
+        }
+
+        if (timeSinceFire >= fireDelay)
+        {
+            Vector2 pos = (target.rigidbody2D.position - rigidbody2D.position).normalized;
+
+            for (int i = 0; i < 360; i+= increaseAngle)
+            {
+                fire(calcOffset(pos, i + fireAngle));
+            }
+            fireAngle += increment;
+            timeSinceFire = 0;
+        }
+        timeSinceFire++;
+    }
+
+    public override void fire(Vector2 direction)//,Color color)
+    {
+        Quaternion rot = Quaternion.identity;
+        Bullet shell = ((GameObject)Instantiate(bullet, rigidbody2D.position, rot)).GetComponent<Bullet>();
+        shell.target = direction;
+    }
+}
