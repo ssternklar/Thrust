2e0da442f1f5a6f0a01122d64b2acd8e207f81c4
Peter McNamara <pmm9335@rit.edu>
2014-11-14 20:15:21 -0500

    Added a starfield picture, started the scrolling logic for it

---
+++ Assets/Scripts/StarfieldScrolling.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarfieldScrolling : MonoBehaviour {
+
+    public float Speed = 0;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody.velocity = new Vector2(0, Speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.MovePosition(rigidbody.position + rigidbody.velocity);
+        
+        if(rigidbody.position.y > camera.orthographicSize)
+        {
+            rigidbody.MovePosition(new Vector2(rigidbody.position.x, -camera.orthographicSize));
+        }
+	}
+}
