b9a3230d6f75ae6ecd02f0732ef159e50eb3c5a2
Matthew Fobare <mf7753@g.rit.edu>
2014-11-15 08:47:42 -0500

    Implemented a text for life counter. Currently doesn't quite work.

--- Assets/Scripts/Core/LifeCounter.cs
+++ Assets/Scripts/Core/LifeCounter.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class LifeCounter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject shipSprite;
+    public GameObject lifeText;
+    private ShipMovement sMovement;
+    private int lives;
+
+    // Use this for initialization
+    void Start()
+    {
+        sMovement = GetComponent<ShipMovement>();        
+        lives = sMovement.lives;
+        lifeText.guiText.text = "Lives " + lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives = sMovement.lives;
+
+        lifeText.guiText.text = "Lives " + lives;
+    }
 }
