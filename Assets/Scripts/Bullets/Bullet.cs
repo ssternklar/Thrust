9ac80d35176c16f2f7c52f6b15277b691eab0043
Peter McNamara <pmm9335@rit.edu>
2014-11-15 03:20:01 -0500

    Half implemented some angle stuff, then went for food.

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -9,15 +9,27 @@ public class Bullet : MonoBehaviour {
     public float lifeTimer;
     public float timeAlive;
 
+    //Used for curved shots only
+    public float angle = 0;
+    public float rotationalVelocity = 0;
+
 	// Use this for initialization
 	void Start () 
     {
         controller = GetComponent<Controller>();
+        //angle = Vector2.Angle(, target);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        if(rotationalVelocity!=0)
+        {
+            angle += rotationalVelocity;
+            target = new Vector2(Mathf.Cos(angle),Mathf.Sin(angle));
+        }
+
+
         controller.Translate(target*5);
 
         if (timeAlive >= lifeTimer)
