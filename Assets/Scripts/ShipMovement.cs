b4737d8d8760caa40738c2c67e5188819d9dc87d
Samuel Sternklar <ssternklar@gmail.com>
2014-11-14 20:00:20 -0500

    Updated Controller.cs to work the way we want it to. Added ShipMovement.cs for ship movement.

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller))]
+public class ShipMovement : MonoBehaviour {
+
+    public Vector2 offset;
+    Controller controller;
+
+    public void Start()
+    {
+        controller = GetComponent<Controller>();
+    }
+
+    public void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            Vector2 position = new Vector2();
+            foreach(Touch touch in Input.touches)
+            {
+                position += touch.position;
+            }
+            position /= Input.touchCount;
+            position += offset;
+
+            if(position.x > Screen.width - controller.SpriteHalfWidth)
+            {
+                position.x = Screen.width - controller.SpriteHalfWidth;
+            }
+
+            else if(position.y > Screen.height - controller.SpriteHalfHeight)
+            {
+                position.x = Screen.height - controller.SpriteHalfHeight;
+            }
+
+            else if(position.x < controller.SpriteHalfWidth)
+            {
+                position.x = controller.SpriteHalfWidth;
+            }
+
+            else if(position.y < controller.SpriteHalfHeight)
+            {
+                position.y = controller.SpriteHalfHeight;
+            }
+
+            controller.MovePosition(position);
+        }
+    }
+}
