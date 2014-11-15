851038f5786e3fb1235016384a10504cdf74a2d7
unknown <sis2773@GDD-27.main.ad.rit.edu>
2014-11-14 19:20:47 -0500

    Added generic controller

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	protected virtual void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	
+	}
+	
+	//Translates by "velocity" amount
+	public virtual void Translate(Vector3 velocity)
+	{
+		rigidbody.MovePosition(rigidbody.position + velocity);
+	}
+	
+	/// <summary>
+    /// Directly sets position to "position"
+	/// </summary>
+	/// <param name="position">Thing to set position to</param>
+	public virtual void MovePosition(Vector3 position)
+	{
+		rigidbody.MovePosition(position);
+	}
+
+    /// <summary>
+    /// Destroys the GameObject
+    /// </summary>
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
