ca3ed9a670244f41cb120031b94138a558a72a80
Wesley Waligora <westleywaligora@gmail.com>
2014-11-14 22:38:23 -0500

    Created the BulletPattern script which is abstract and holds the abstract fire method. Created the StraightFire script which should fire directly at the player, but currently does nothing. Uploading my debug state for better debugging.

---
+++ Assets/Scripts/BulletPattern.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BulletPattern : MonoBehaviour {
+
+    Vector2 target;
+
+    public float fireDelay;
+    public float timer;
+    
+    public void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public abstract void fire();
+}
