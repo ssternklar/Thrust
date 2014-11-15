870044f8118cd44e92ba8c78cb7df47381cb53cb
Peter McNamara <pmm9335@rit.edu>
2014-11-14 19:44:34 -0500

    Made a basic set of enemy behavior. Currently unfinished due to dependency on prefabs

---
+++ Assets/Scripts/BasicEnemyBehavior.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyBehavior : MonoBehaviour {
+
+    public float MaxSpeed = 1.5f;
+    private float shootTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody.velocity = new Vector2(0, -MaxSpeed);
+        shootTimer = Random.Range(30, 100);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Movement
+	    //call alignX on the player prefab
+
+        //Attacking
+        shootTimer -= 1;
+        if (shootTimer<0)
+        {
+            shootTimer = Random.Range(30, 100);
+            Shoot();
+        }
+	}
+
+    /// <summary>
+    /// Makes the object set it's xSpd to chase a target
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="buffer"></param>
+    void AlignX(GameObject target, float buffer)
+    {
+        //If we're to the right, move left
+        if(rigidbody.position.x > target.rigidbody.position.x + buffer)
+        {
+            rigidbody.velocity = new Vector2(-MaxSpeed, rigidbody.velocity.y);
+        }
+        //If we're to the left, move right
+        else if (rigidbody.position.x < target.rigidbody.position.x - buffer)
+        {
+            rigidbody.velocity = new Vector2(MaxSpeed, rigidbody.velocity.y);
+        }
+        //else decelerate
+        else
+        {
+            rigidbody.velocity *= .8f;
+        }
+        
+    }//end alignX
+
+    void Shoot()
+    {
+        //create a bullet prefab and make it move and things.
+    }
+}
