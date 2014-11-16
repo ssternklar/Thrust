baba8cd09e952cc33903a31585c32f813b984c93
Matthew Fobare <mf7753@g.rit.edu>
2014-11-15 19:35:23 -0500

    Added Ship Sprite and replaced Life Counter text with images.

--- Assets/Scripts/Core/LifeCounter.cs
+++ Assets/Scripts/Core/LifeCounter.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LifeCounter : MonoBehaviour {
-
-    public GameObject shipSprite;
+        
     public GameObject lifeText;
+    public GameObject shipSprite;
     private ShipMovement sMovement;
+    private List<GameObject> livesArray;
 
     // Use this for initialization
     void Start()
-    {
+    {        
         sMovement = GetComponent<ShipMovement>();
         lifeText = GameObject.Find("GUI Text");
-        lifeText.guiText.text = "Lives " + sMovement.lives;
+        lifeText.guiText.text = "Lives: ";
+        livesArray = new List<GameObject>();
+
+        float off = 0;        
+        for (int i = 0; i < sMovement.lives; i++)
+        {
+            livesArray.Add((GameObject)Instantiate(shipSprite));
+            livesArray[i].transform.position = new Vector2(-1.5f + off, 4.75f);
+            off += 0.6f;
+        }        
     }
 
     // Update is called once per frame
     void Update()
     {
-        lifeText.guiText.text = "Lives " + sMovement.lives;
+        lifeText.guiText.text = "Lives: ";
     }
 }
